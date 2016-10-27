0c50eee44f64ea947016fed211fc83941b20b5aa
AleksF92 <Aleks_F92@hotmail.com>
2016-10-28 01:32:52 +0200

    Project created
    + Base and drone models created.

---
+++ Veins/Assets/Scripts/Gizmos/DisplayForward.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayForward : MonoBehaviour {
+    void OnDrawGizmos() {
+        float lineLength = 0.5f;
+        Vector3 targetPos = transform.position + (transform.forward * lineLength);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, targetPos);
+    }
+}
